f6fe97095b08482846fcd4ce0c7db8faf3ac8aa0
eglatzer <eglatzer@gmail.com>
2016-03-01 13:09:23 +0200

    updated and added files

---
+++ spring/src/main/java/com/ilad/spring/SpellChecker.java
@@ -0,0 +1,12 @@
+package com.ilad.spring;
+
+public class SpellChecker {
+	// Constructor
+	public SpellChecker() {
+		System.out.println("Inside SpellChecker constructor.");
+	}
+	
+	public void checkSpelling() {
+		System.out.println("Inside checkSpelling.");
+	}
+}
