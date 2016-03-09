d1259deebfa2ff6b0f6d5590d4f720de383532c4
eglatzer <eglatzer@gmail.com>
2016-03-09 12:55:26 +0200

    updated files

--- selenium/src/com/ilad/selenium/GoogleSearch.java
+++ selenium/src/com/ilad/selenium/GoogleSearch.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class GoogleSearch  {
-    public static void main(String[] args) throws InterruptedException {
-        // Create a new instance of the Firefox driver
+public class GoogleSearch {
+	public static void main(String[] args) throws InterruptedException {
+		// Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface, 
         // not the implementation.
         WebDriver driver = new FirefoxDriver();
@@ -46,5 +46,5 @@ public class GoogleSearch  {
         
         //Close the browser
         driver.quit();
-    }
+	}
 }
